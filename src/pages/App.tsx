938d763654a93c8a6913e7a41d3670bd3eb69f12
Astrozera <fascioantonio@gmail.com>
2023-08-08 15:43:21 -0300

    props adicionado

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Cronometro from '../components/Cronometro';
 import Formulario from '../components/Formulario';
 import Lista from '../components/Lista';
 import style from './App.module.scss'
@@ -7,8 +8,9 @@ import style from './App.module.scss'
 function App() {
   return (
     <div className={style.AppStyle}>
-      < Formulario />
-      <Lista />
+      <Formulario/>
+      <Lista/>
+      <Cronometro/>
     </div>
   );
 }
